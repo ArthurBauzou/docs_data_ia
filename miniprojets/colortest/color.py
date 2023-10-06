341d6e7d6443b3650b852bc82fb31181beb6d7d2
arthurbauzou <arthurbauzou@gmail.com>
2023-10-06 09:41:23 +0200

    color dataframe crée

--- miniprojets/colortest/color.py
+++ miniprojets/colortest/color.py
@@ -1,8 +1,29 @@
-def colorInterval(colorA:str, colorB:str, n:int=3) -> list(str):
-    '''
-    prend deux couleurs en entrée et retourne un tableau des couleurs intermédiaires
-    '''
-    if n < 1 or n > 10 :
-        raise Exception("n doit être entre 1 et 10")
-    
-    pass
+# farbriquer un gradient de couleur
+
+def hexinterval(hex_start, hex_end, n):
+    n = n+1
+    start = int(hex_start, 16)
+    end = int(hex_end, 16)
+    step = abs(end - start) / n
+
+    if end > 255: raise Exception('pas de valeur supérieures à FF')
+
+    res = [hex_start]
+
+    for i in range(1,n+1):
+        val = start + int(i*step) if end > start else start - int(i*step)
+        val = '{:02x}'.format(val)
+        res.append(val)
+
+    return res
+
+def colorlist(color1, color2, n=4):
+    reds = hexinterval(color1[1:3], color2[1:3], n)
+    greens = hexinterval(color1[3:5], color2[3:5], n)
+    blues = hexinterval(color1[5:7], color2[5:7], n)
+
+    return ['#'+x+y+z for x,y,z in zip(reds, greens, blues)]
+
+
+
+print(colorlist('#5f402f', '#e4b6b6'))
